5431156df3ee2ea6adb6d4b8deed6ad8962e03aa
PravNJ <prav.nj@gmail.com>
2021-08-19 10:37:03 +1000

    messy testing python to julia and vice versa

---
+++ gridlines.jl
@@ -0,0 +1,13 @@
+using JLD2, FileIO, SharedArrays
+
+Δλ = 7 
+#subordinate_line_vac = 6105.7
+li_vac = 6709.702
+wls = Float32.(load("wl_grid.jld2")["wl_grid"])
+subordinate_line_mask = subordinate_line_vac - Δλ .< wls .< subordinate_line_vac + Δλ
+npix = sum(.! subordinate_line_mask) #n pixels that are not masked 
+wl_grid = SharedArray{Float32}(npix) 
+wl_grid .= wls[.! subordinate_line_mask]
+line_mask = li_vac - Δλ .< wl_grid .< li_vac + Δλ
+
+
